08d4bdc053d9fe249ae82845c2389ce3d590559b
José Gaspar <jad.gaspar@gmail.com>
2017-09-04 13:03:51 +0100

    added new optional arguments for visibility, message retention and (#1)
    receive wait time

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 resource "aws_sqs_queue" "q" {
-  name                      = "${var.name}"
-  
-  delay_seconds             = "${var.delay_seconds}"
-  max_message_size          = "${var.max_message_size}"
-  
-  redrive_policy            = "${var.redrive_policy}"
-  policy                    = "${var.policy}"
+  name = "${var.name}"
+
+  delay_seconds              = "${var.delay_seconds}"
+  max_message_size           = "${var.max_message_size}"
+  visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
+  message_retention_seconds  = "${var.message_retention_seconds}"
+  receive_wait_time_seconds  = "${var.receive_wait_time_seconds}"
+
+  redrive_policy = "${var.redrive_policy}"
+  policy         = "${var.policy}"
 }
